4166e053443a63e574a95e8f45795b51df6fe071
anttko <anttko@>
2023-01-13 22:05:51 +0200

    remove console.log's

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
-import Head from 'next/head'
 import useSwr from 'swr'
-import Link from 'next/link'
 import DroneCard from '../components/droneCard'
 import { Grid, Box, Heading } from "@chakra-ui/react";
 import { iDrone } from '../interfaces/types';
+
 const fetcher = (url: string) => fetch(url).then((res) => {
   if (!res.ok) {
     throw new Error('Someting went wrong')
@@ -13,11 +12,12 @@ const fetcher = (url: string) => fetch(url).then((res) => {
 )
 export default function Home() {
   const { data, error } = useSwr('/api/drones', fetcher, {
-    refreshInterval: 10000
+    refreshInterval: 5000
   })
   if (error) return <div>Failed to load drones</div>
   if (!data) return <div>Loading...</div>
-  console.log(data)
+
+
   return (
     <div>
       <Heading display="flex" justifyContent="center" alignItems="center" m={10}>Drones that violate the NFZ</Heading>
@@ -28,8 +28,9 @@ export default function Home() {
           {data || data.lenght >= 1 ? data.map((item: iDrone, i: number) => {
             const ix = item.positionX / 1000
             const iy = item.positionY / 1000
-            console.log(ix, iy)
-            return <circle key={i} cx={ix} cy={iy} r={3} />
+            return (
+              <circle key={i} cx={ix} cy={iy} r={3} />
+            )
           }) : null}
           <circle cx={250} cy={250} r={100} fill="grey" fillOpacity="0.4" />
           <text x={250} y={250} textAnchor="middle" dominantBaseline="central" style={{ fontSize: 20 }}>
